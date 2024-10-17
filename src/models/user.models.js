8f061af1410cf9977e3363e96cf5a0e008e5a183
tariq615 <iqbaltariq615@gmail.com>
2024-10-17 12:31:56 +0300

    user, video models

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const UserSchema = new Schema(
   {
@@ -49,4 +51,43 @@ const UserSchema = new Schema(
   }
 );
 
+UserSchema.pre("save", async function (next) {
+  // using 'pre hook' of mongoose
+  if (!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+UserSchema.methods.isPasswordCorrect = async function (password) {
+  //adding method by using 'methods', creating method for checking password
+  return await bcrypt.compare(password, this.password); // first password fom user field, second from database
+};
+
+UserSchema.methods.generateAccessToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+UserSchema.methods.generateRefreshToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 export const User = mongoose.model("User", UserSchema);
